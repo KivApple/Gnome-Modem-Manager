d42bc83e7b7e6e428824a6d73922a3b111397515
KIV <kiv.apple@gmail.com>
2012-07-08 18:03:46 +0400

    Fix debian build script

--- packaging/debian/build.sh
+++ packaging/debian/build.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+DEBIANDIR=`pwd`
 cd ../..
-make
-mkdir package
-fakeroot make install DESTDIR=package
 CURDIR=`pwd`
-ln -s $CURDIR/packaging/debian/info $CURDIR/package/DEBIAN
+make
+mkdir $CURDIR/package
+fakeroot make install DESTDIR=$CURDIR/package
+BINSIZE=`du -s $CURDIR/package | grep  -o [0-9]*`
+cp -r $DEBIANDIR/info $CURDIR/package/DEBIAN
+cat $CURDIR/package/DEBIAN/control.template | sed "s/\$BONSIZE/$BINSIZE/g" > $CURDIR/package/DEBIAN/control
+rm -f $CURDIR/package/DEBIAN/control.template
 fakeroot dpkg -b package
 mv "package.deb" "packaging/debian/gnome-modem-manager.deb"
 rm -rf $CURDIR/package
