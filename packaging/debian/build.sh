0194394dd909972081bee84371df7a3915f6fd97
KIV <kiv.apple@gmail.com>
2012-07-08 18:40:18 +0400

    Auto architecture detecting in Debian build script

--- packaging/debian/build.sh
+++ packaging/debian/build.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
+ARCH=`uname -m | sed "s/i686/i386/" | sed "s/x86_64/amd64/"`
 DEBIANDIR=`pwd`
 cd ../..
 CURDIR=`pwd`
 make
 mkdir $CURDIR/package
 fakeroot make install DESTDIR=$CURDIR/package
-BINSIZE=`du -s $CURDIR/package | grep  -o [0-9]*`
+BINSIZE=`du -s ./package | grep  -o [0-9]*`
 cp -r $DEBIANDIR/info $CURDIR/package/DEBIAN
-cat $CURDIR/package/DEBIAN/control.template | sed "s/\$BINSIZE/$BINSIZE/g" > $CURDIR/package/DEBIAN/control
+cat $CURDIR/package/DEBIAN/control.template | sed "s/\$BINSIZE/$BINSIZE/g" | sed "s/\$ARCH/$ARCH/g" > $CURDIR/package/DEBIAN/control
 rm -f $CURDIR/package/DEBIAN/control.template
 fakeroot dpkg -b package
-mv "package.deb" "packaging/debian/gnome-modem-manager.deb"
+mv "package.deb" "packaging/debian/gnome-modem-manager-$ARCH.deb"
 rm -rf $CURDIR/package
