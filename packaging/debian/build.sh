e00e927368f93ef417aa2d671d31ca9ff1d65e90
KIV <kiv.apple@gmail.com>
2012-07-08 18:07:03 +0400

    build.sh hotfix

--- packaging/debian/build.sh
+++ packaging/debian/build.sh
@@ -7,7 +7,7 @@ mkdir $CURDIR/package
 fakeroot make install DESTDIR=$CURDIR/package
 BINSIZE=`du -s $CURDIR/package | grep  -o [0-9]*`
 cp -r $DEBIANDIR/info $CURDIR/package/DEBIAN
-cat $CURDIR/package/DEBIAN/control.template | sed "s/\$BONSIZE/$BINSIZE/g" > $CURDIR/package/DEBIAN/control
+cat $CURDIR/package/DEBIAN/control.template | sed "s/\$BINSIZE/$BINSIZE/g" > $CURDIR/package/DEBIAN/control
 rm -f $CURDIR/package/DEBIAN/control.template
 fakeroot dpkg -b package
 mv "package.deb" "packaging/debian/gnome-modem-manager.deb"
