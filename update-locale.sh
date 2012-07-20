b30c1d35507802bb5a181cd32b9e09faf251fdb1
KIV <kiv.apple@gmail.com>
2012-07-20 16:32:53 +0400

    Update update-locale.sh

--- update-locale.sh
+++ update-locale.sh
@@ -2,5 +2,5 @@
 xgettext --language=Glade -o po/gnome-modem-manager.pot gnome-modem-manager.ui
 msgmerge po/gnome-modem-manager-ru_RU.po po/gnome-modem-manager.pot > /tmp/ru_RU.po
 mv /tmp/ru_RU.po po/gnome-modem-manager-ru_RU.po
-msgmerge po/gnome-modem-manager-ru_UA.po po/gnome-modem-manager.pot > /tmp/ru_UA.po
-mv /tmp/ru_UA.po po/gnome-modem-manager-ru_UA.po
+msgmerge po/gnome-modem-manager-ua_UA.po po/gnome-modem-manager.pot > /tmp/ua_UA.po
+mv /tmp/ua_UA.po po/gnome-modem-manager-ua_UA.po
